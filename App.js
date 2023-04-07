b159be7fd79b11c23ebf812891d4a87275a6bbab
João da Cruz <102554786+JohnDaCruz@users.noreply.github.com>
2023-04-07 09:27:06 -0300

    Status bar

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
 import Main from './src/components/Advice/main'
 
 export default function App() {
 
   return (
+
     <View style={styles.container}>
-      <Main/>
+      <StatusBar style={"dark"} backgroundColor="#F5F5F5" />
+      <Main />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:'column',
+    flexDirection: 'column',
     backgroundColor: '#FFBD44',
     alignItems: 'center',
     justifyContent: 'center'
